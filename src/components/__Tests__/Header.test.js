a5adf7c846baea04c3e10e89ccb96cfc006d5d02
Reyhana Cherop <reyhanacynthia@gmail.com>
2023-08-19 13:07:39 +0300

    TDD and folder arrangement

--- src/components/__Tests__/Header.test.js
+++ src/components/__Tests__/Header.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Header from '../Header';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<Header />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// it('renders without crashing', () => {
+//     const div = document.createElement('div');
+//     ReactDOM.render(<Header />, div);
+// })
